b8f32e5e37a1a6784a725bce451131759a23f19c
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-05-13 20:21:52 -0700

    added dependancies, SVG

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-    Icon
+  <div class="icon-wrapper">
+    <svg 
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <use v-bind="{'xlink:href':'/feather-sprite.svg#'+name}" />
+    </svg>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    name: String, // the name of the symbol we want to use
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
