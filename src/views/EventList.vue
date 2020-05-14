2b56a5bbf2d58bc67f5e1a1883342261dfd06822
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-05-14 12:06:04 -0700

    added axios, making axios call to our mock API and setting a data attribute called events to the returned API data

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -7,10 +7,25 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue';
+import axios from 'axios';
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios.get('http://localhost:3000/events')
+    .then(response => {
+      this.events = response.data
+    })
+    .catch(error => {
+      console.log('Error:' + error.response)
+    })
   }
 }
 </script>
